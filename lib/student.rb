f63a95df6af14fdaa08b9e641382f88ab6dc7278
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-26 12:39:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,10 +7,10 @@ attr_accesor :name, :grade
   end
 
   def save
-
+    DB[:conn].execute("INSERT INTO students VALUES (?,?,?)", id, self.name, self.grade)
     
   end
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
 end
