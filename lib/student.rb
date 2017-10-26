a3f6443de538b07430c9cfc8bab6127d6a6dcf60
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-26 12:50:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,10 +8,17 @@ attr_accessor :name, :grade
 
   def self.create_table
   sql = <<-SQL
-        CREATE TABLE students
+        CREATE TABLE IF NOT EXISTS students
         (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)
         SQL
   DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+        DROP TABLE students
+        SQL
+    DB[:conn].execute(sql)  
   end
 
   def save
