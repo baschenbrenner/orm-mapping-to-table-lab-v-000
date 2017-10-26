1672c81d8bb3cf4cba9bed51d8c72cf163843980
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-26 12:38:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,15 @@
 class Student
+attr_accesor :name, :grade
 
+  def initialize(name, grade)
+    @name=name
+    @grade=grade
+  end
+
+  def save
+
+    
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
